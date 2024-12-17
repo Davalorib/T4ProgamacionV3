1faf934d94a1bac550b68942f1e4e9c8b9170038
PC-CHAMOY\daval <davalorib@gmail.com>
2024-12-17 12:55:46 +0100

    a falta del junit

--- T4ProgramacionV3/src/main/java/T4Programacion/BingoAzahara.java
+++ T4ProgramacionV3/src/main/java/T4Programacion/BingoAzahara.java
@@ -3,16 +3,30 @@ package T4Programacion;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
+/**
+ * Esta clase contiene los métodos necesarios para resolver el problema 729 del Concurso ProgramaMe
+ * @author David Alonso
+ * @version 1.0 16/12/2002
+ * @see <a href = "https://aceptaelreto.com/problem/statement.php?id=729" /> El bingo de Azahara </a>
+ */
 public class BingoAzahara {
 
     static Scanner ent;
 
+    /**
+     * El main de la manera que les gusta a estos.
+     * @param args  pues el vector que hace que el main sea el main.
+     */
     public static void main(String[] args) {
         ent = new Scanner(System.in);
         while (casoDePrueba()) {
         }
     }
 
+    /**
+     * Este método contiene el código del programa, va llamando a los demás métodos.
+     * @return true/false - devuelve un booleano para que el main siga ejecutando el programa o termine.
+     */
     public static boolean casoDePrueba() {
 
         int n = numero();
@@ -34,6 +48,11 @@ public class BingoAzahara {
         }
     }
 
+    /**
+     * Este método es para saber cuántos cartones vamos a tener, si dicho número es 0 o un carácter no válido, el programa terminará.
+     * Luego lo reutilizamos para las bolas.
+     * @return num - devuelve un número introducido por teclado, si es un carácter no válido devuelve un 0.
+     */
     public static int numero() {
 
         try {
@@ -47,6 +66,11 @@ public class BingoAzahara {
 
     }
 
+    /**
+     * Este método es para comprobar que estamos dentro del rango que nos dicta el enunciado.
+     * @param n es una variable de tipo int que contiene el número de cartones que vamos a tener.
+     * @return true/false - devuelve un booleano true/false dependiendo si se encuentra o no dentro de nuestro rango.
+     */
     public static boolean comprobar(int n){
 
         if (n < 1 || n > 50000) {
@@ -56,6 +80,11 @@ public class BingoAzahara {
         }
     }
 
+    /**
+     * Este método es para pedir y almacenar los nombres y números de los cartones del bingo.
+     * @param n es una variable de tipo int que contiene el número de cartones que vamos a tener.
+     * @return matriz - devuelve una matriz con los datos de los cartones introducidos por teclado.
+     */
     public static String[][] carton(int n){
 
         String datos;
@@ -69,6 +98,11 @@ public class BingoAzahara {
         return matriz;
     }
 
+    /**
+     * Este método es para pedir y almacenar las bolas del bingo.
+     * @param n1 es una variable de tipo int que contiene el número de bolas que vamos a tener.
+     * @return bolas - devuelve un vector con los números de las bolas que han salido.
+     */
     public static int[] bolas(int n1){
 
         int[] bolas = new int[n1];
@@ -81,6 +115,13 @@ public class BingoAzahara {
         return bolas;
     }
 
+    /**
+     * Este método es para comparar los cartones con las bolas y sacar los posibles ganadores del bingo.
+     * @param matriz es una matriz de tipo String que contiene datos de los cartones.
+     * @param bolas es un vector de tipo int que contiene los números de las bolas que han salido en el bingo.
+     * @param n es una variable de tipo int que contiene el número de cartones que tenemos.
+     * @return ganadores - devuelve un vector con los nombres de los ganadores del bingo.
+     */
     public static String[] ganadores(String[][] matriz, int[] bolas, int n) {
 
         String[] genios = new String[n];
@@ -125,12 +166,18 @@ public class BingoAzahara {
         return ganadores;
     }
 
+    /**
+     * Este método es para ordenar los nombres de los ganadores alfabéticamente.
+     * @param pibes es un vector de tipo String que contiene los nombres de los ganadores del bingo.
+     */
     public static void ordenar(String[] pibes) {
 
         String aux;
 
         for (int i = 0; i < pibes.length; i++) {
             for (int j = 0; j < pibes.length-i-1; j++) {
+                //el compareTo compara cadenas y objetos, si son cadenas lo hace en orden alfabético
+                //si el actual va antes que el siguiente da un valor negativo, si el actual va después da uno positivo, si es igual un 0
                 if (pibes[j].compareTo(pibes[j + 1]) > 0) {
                     aux = pibes[j];
                     pibes[j] = pibes[j + 1];
@@ -140,6 +187,10 @@ public class BingoAzahara {
         }
     }
 
+    /**
+     * Este método es para imprimir por pantalla los nombres de los ganadores ordenados.
+     * @param ganadores es un vector de tipo String que contiene los nombres de los ganadores del bingo.
+     */
     public static void imprimir(String[] ganadores) {
 
         for (int i = 0; i < ganadores.length; i++) {
@@ -150,5 +201,5 @@ public class BingoAzahara {
         }
         System.out.println();
     }
-}
 
+}
--- T4ProgramacionV3/src/test/java/T4Programacion/BingoAzaharaTest.java
+++ T4ProgramacionV3/src/test/java/T4Programacion/BingoAzaharaTest.java
@@ -2,14 +2,37 @@ package T4Programacion;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 class BingoAzaharaTest {
 
     @Test
-    void casoDePrueba() {
-    }
-
-    @Test
     void numero() {
+
+        //simulamos que el usuario ingresa caracteres
+        String entrada = "a";
+        System.setIn(new ByteArrayInputStream(entrada.getBytes()));
+
+        //creamos un ByteArrayOutputStream para guardar la salida por consola
+        ByteArrayOutputStream salida = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(salida));
+
+        BingoAzahara prueba = new BingoAzahara();
+
+        //llamamos al metodo que procesa la entrada
+        int resultado = prueba.numero();
+
+        //verificamos que el valor que devuelve es el esperado (-1)
+        assertEquals(0, resultado);
+
     }
 
     @Test
